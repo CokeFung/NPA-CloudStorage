e53dcc1547d31b410eebf8aaf3680478cd63645c
Fung <naruebetcm@gmail.com>
2021-05-07 22:26:38 +0700

    add responsive css to container storage

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className='App-logo' alt='app-logo' />
-        <p>
+        <p className='Container-logo'>
           NPA - Cloud Storage
         </p>
         <DraggerContainer />
